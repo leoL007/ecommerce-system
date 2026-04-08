e0bfdbd228a36c88336ca0a8f224caeea95ef154
LeoL007 <leoliu.tech@qq.com>
2026-04-08 11:17:03 +0800

    init ecommerce system

---
+++ ecommerce-backend/src/main/java/com/example/ecommerce/controller/HelloController.java
@@ -0,0 +1,16 @@
+﻿package com.example.ecommerce.controller;
+
+import com.example.ecommerce.common.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Result<String> hello() {
+        return Result.success("hello backend");
+    }
+}
